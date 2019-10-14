86093431bd45531562f5e33b9830178ce4375385
valeros <valeros@bigmir.net>
2019-10-14 20:35:46 +0300

    Use unix path separator when uploading via openocd

--- builder/main.py
+++ builder/main.py
@@ -214,8 +214,9 @@ elif upload_protocol in debug_tools:
     openocd_args.extend(
         debug_tools.get(upload_protocol).get("server").get("arguments", []))
     openocd_args.extend([
-        "-c", "program {$SOURCE} %s verify reset; shutdown;" %
-        board.get("upload.offset_address", "0x8000000")
+        "-c", "program %s %s verify reset; shutdown;" % (
+            env.subst(upload_source)[0].replace("\\", "/"), board.get(
+                "upload.offset_address", "0x8000000"))
     ])
     openocd_args = [
         f.replace("$PACKAGE_DIR",
