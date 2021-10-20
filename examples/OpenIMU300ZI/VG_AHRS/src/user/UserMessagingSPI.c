90a6c3b97304964357de355f8c144714945dc125
Akira SHIMAHARA <akira215corp@gmail.com>
2021-10-21 00:11:33 +0200

    Correcting header case spelling

--- examples/OpenIMU300ZI/VG_AHRS/src/user/UserMessagingSPI.c
+++ examples/OpenIMU300ZI/VG_AHRS/src/user/UserMessagingSPI.c
@@ -35,7 +35,7 @@ limitations under the License.
 #include "spiAPI.h"
 #include "magAPI.h"
 #include "appVersion.h"
-#include "BitStatus.h"
+#include "BITStatus.h"
 #include "EKF_Algorithm.h"
 
 uint8_t  _spiDataBuf[2][SPI_DATA_BUF_LEN];
